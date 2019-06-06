c9ec67358dd3f727a5f539ba2b9649b4a2f1a66d
Yan Vugenfirer <yan@daynix.com>
2019-06-06 17:34:40 +0300

    Add test code to pthread test
    Signed-off-by: Yan Vugenfirer <yan@daynix.com>

--- test_pthread/test_pthread.c
+++ test_pthread/test_pthread.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <pthread.h>
+
+void * test_thread(void * message)
+{
+	printf("We are in thread\n");
+
+	return NULL;
+}
 
 int main()
 {
-	printf ("Test pthreads.\n");
+	pthread_t thread;
+
+	printf ("Test pthread.\n");
+
+	if(pthread_create(&thread, NULL, test_thread, NULL) != 0) {
+		printf("pthread_create failed!\n");
+		return -1;
+	}
+
+	pthread_join(thread, NULL);
+
 	return 0;
 }
